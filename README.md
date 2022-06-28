cb88d9c89c8d8414342cf53267152533e423578c
Jan Stümmel <jan@ubari.de>
2022-06-28 15:35:41 +0200

    initial

---
+++ README.md
@@ -0,0 +1,19 @@
+## Switch theme
+
+a firefox extension to switch between light/dark theme.
+
+### Development
+
+```sh
+npm i
+npm start
+```
+
+### Credits
+
+* Icon from [tabler/tabler-icons](https://github.com/tabler/tabler-icons)
+* CSS from [kognise/water.css](https://github.com/kognise/water.css)
+
+### License
+
+MIT
---
+++ src/popup.js
@@ -0,0 +1,54 @@
+const lightThemesSelect = document.getElementById('light-themes');
+const darkThemesSelect = document.getElementById('dark-themes');
+const switchThemesButton = document.getElementById('switch-themes');
+
+function getThemes() {
+  return browser.management.getAll().then(extensions => 
+    extensions.filter((extension) => extension.type === 'theme')
+  );
+}
+
+async function appendSelectOptions(node, themes, theme) {
+  const themeId = await getSelectedTheme(theme);
+  themes.forEach((theme) => {
+    const option = document.createElement('option');
+    option.textContent = theme.name;
+    option.value = theme.id;
+    option.selected = themeId == theme.id;
+    node.appendChild(option);
+  })
+}
+
+function getSelectedTheme(key) {
+  return browser.storage.local.get().then(res => res[key])
+}
+
+function saveSelectTheme(key) {
+  return function(evt) {
+    evt.preventDefault();
+    browser.storage.local.set({
+      [key]: evt.target.value,
+    });
+  };
+}
+
+async function switchTheme(evt) {
+  evt.preventDefault();
+  const matchesDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const theme = await getSelectedTheme(matchesDark ? 'light' : 'dark');
+  if (theme) {
+    await browser.management.setEnabled(theme, true);
+  }
+  window.close()
+}
+
+async function init() {
+  const themes = await getThemes();
+  appendSelectOptions(lightThemesSelect, themes, 'light');
+  appendSelectOptions(darkThemesSelect, themes, 'dark');
+}
+
+document.addEventListener('DOMContentLoaded', init);
+lightThemesSelect.addEventListener('change', saveSelectTheme('light'));
+darkThemesSelect.addEventListener('change', saveSelectTheme('dark'));
+switchThemesButton.addEventListener('click', switchTheme);
